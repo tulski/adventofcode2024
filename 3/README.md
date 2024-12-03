2bec150d7c6d241fa64ed389d1f588c31a814816
tulski <me@tulski.com>
2024-12-03 20:43:27 +0100

    Day 3: part 2

--- 3/README.md
+++ 3/README.md
@@ -1,8 +1,9 @@
 ```text
-➜ npm run start:3
+$ npm run start:3
 
 > @tulski/adventofcode2024@0.0.3 start:3
 > tsx index.ts
 
-Result:  167090022
+Part one:  167090022
+Part two:  89823704
 ```
--- 3/index.ts
+++ 3/index.ts
@@ -1,13 +1,22 @@
 import {readFile} from "fs/promises";
 
 const FILE_PATH = 'input.txt';
-const SEARCH_PATTERN = /mul\((\d{1,3}),(\d{1,3})\)/gm;
+const PART_ONE_SEARCH_PATTERN = /mul\((\d{1,3}),(\d{1,3})\)/gm;
+const PART_TWO_DISABLED_PARTS_PATTERN = /don't\(\)([\s\S]*?)do\(\)/g;
 
-const fileContent = await readFile(FILE_PATH, 'utf-8');
+const input = await readFile(FILE_PATH, 'utf-8');
 
-const matches = [...fileContent.matchAll(SEARCH_PATTERN)]
-    .map(([match, group1, group2]: any[]) => ([+group1, +group2]));
+const partOne = (input: string): number => {
+    const matches = [...input.matchAll(PART_ONE_SEARCH_PATTERN)]
+        .map(([match, group1, group2]: any[]) => ([+group1, +group2]));
 
-const result = matches.reduce((acc, [a, b]) => acc + (a * b), 0);
+    return matches.reduce((acc, [a, b]) => acc + (a * b), 0);
+}
 
-console.log('Result: ', result);
+const partTwo = (input: string): number => {
+    const onlyEnabled = input.replaceAll(PART_TWO_DISABLED_PARTS_PATTERN, '');
+    return partOne(onlyEnabled);
+}
+
+console.log('Part one: ', partOne(input));
+console.log('Part two: ', partTwo(input));
